7e623917f93c07df1effbba0be4ab0e10de14300
Suren Partheepan <ravinthiranpartheepan1407@gmail.com>
2022-01-08 09:47:06 -0800

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+
+      <h1 className="text-center text-xl"> Ipfs-Web-Client </h1>
+      <h3 className="text-center text-xl"> Developed By Ravinthiran </h3>
+
+          <Component {...pageProps} />
+
+    </div>
+
+  )
 }
 
 export default MyApp
